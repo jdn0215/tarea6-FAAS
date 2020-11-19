5f2e9bb1c42753c655a9589bd158e2fadc87eef3
Daniel Salas <jdn0213@gmail.com>
2020-11-18 20:12:39 -0600

    templates listos

---
+++ www/scripts/app.js
@@ -0,0 +1,63 @@
+const URL = "https://faas-example.netlify.app/api/";
+
+function getAll(entity) {
+    fetch(url + entity)
+        .then((response) => response.json())
+        .then((data) => {
+            fetch('/template/list/' + entity + '.html')
+                .then((response) => response.text())
+                .then((template) => {
+                    var rendered = Mustache.render(template, data);
+                    document.getElementById('content').innerHTML = rendered;
+                });
+        })
+}
+
+function getById(query, entity) {
+    var params = new URLSearchParams(query);
+    fetch(url + entity + '/?id=' + params.get('id'))
+        .then((response) => response.json())
+        .then((data) => {
+            fetch('/template/detail/' + entity + '.html')
+                .then((response) => response.text())
+                .then((template) => {
+                    var rendered = Mustache.render(template, data);
+                    document.getElementById('content').innerHTML = rendered;
+                });
+        })
+}
+
+function home() {
+    fetch('/template/home.html')
+        .then((response) => response.text())
+        .then((template) => {
+            var rendered = Mustache.render(template, {});
+            document.getElementById('content').innerHTML = rendered;
+        });
+}
+
+function init() {
+    router = new Navigo(null, false, '#!');
+    router.on({
+        '/facturas': function() {
+            getAll('facturas');
+        },
+        '/clientes': function() {
+            getAll('clientes');
+        },
+        '/productos': function() {
+            getAll('productos');
+        },
+        '/facturaById': function(_, query) {
+            getById(query, 'facturas');
+        },
+        '/clienteById': function(_, query) {
+            getById(query, 'clientes');
+        },
+        '/productoById': function(_, query) {
+            getById(query, 'productos');
+        }
+    });
+    router.on(() => home());
+    router.resolve();
+}
